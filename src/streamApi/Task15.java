7b3baaa4c6e5b1d29a25f3ec4fb9c54f65434595
Tatsiana <nekhaevna2212@gmail.com>
2023-03-31 11:36:59 +0300

    task

--- src/streamApi/Task15.java
+++ src/streamApi/Task15.java
@@ -1,8 +1,17 @@
 package streamApi;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /**
  * 2.3. Есть стринговый лист фамилий, нужно пройтись по списку, вытащить фамилии которые начинаются на А
  * и поместить их в другой список для дальнейшей работы с ними
  */
 public class Task15 {
+    public static void main(String[] args) {
+        List<String> surname = Stream.of("Андреев", "Петров", "Аркашин", "Ломоносов").collect(Collectors.toList());
+        List<String> selectSurname = surname.stream().filter(p->p.contains("А")).collect(Collectors.toList());
+        System.out.println(selectSurname);
+    }
 }
--- src/streamApi/Task17.java
+++ src/streamApi/Task17.java
@@ -1,7 +1,19 @@
 package streamApi;
 
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
 /**
  * 2.5 Найти сумму нечетных чисел в коллекции.
  */
 public class Task17 {
+    public static void main(String[] args) {
+        List<Integer> list = new Random().ints(10, 1, 20)
+                .boxed()
+                .collect(Collectors.toList());
+
+        long count = list.stream().filter(x -> x % 2 != 0).reduce(Integer::sum).orElse(0);
+
+    }
 }
